955013a2aa7281fa458753628cc231b7c71cb63c
Richard <Richard@MacTheKnife.local>
2017-01-16 19:41:44 -0500

    Initial

---
+++ EMOJI_DICTIONARY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMOJI_DICTIONARY
+//
+//  Created by Richard on 1/16/17.
+//  Copyright © 2017 ELION. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
